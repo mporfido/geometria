953170e1d1d13b30a6d8f9619b516301b6d21044
mporfido <michele.porfido1@gmail.com>
2021-10-10 14:02:47 +0200

    pagina algoritmo di Erone

---
+++ js/ggbHelpFuncs.js
@@ -0,0 +1,9 @@
+function setValore(nome, valore) {
+	var applet = document.ggbApplet;
+	applet.setValue(nome, valore);
+};
+
+function setFase(fase) {
+	var applet = document.ggbApplet;
+	applet.setValue("fase", fase);
+};
